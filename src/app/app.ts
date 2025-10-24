85b8fc4c83f5aa898bcf747f8a8efb0c27b898bc
iShotHarambe123 <tubbiedd@outlook.com>
2025-10-24 12:52:28 +0200

    feat: implement routing and navigation component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
